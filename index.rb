b621f4d3297b13f01a5ac745638d79d8b4418db9
mba <mba@mba.local>
2019-11-27 01:31:23 +0900

    116

--- index.rb
+++ index.rb
@@ -1,28 +1,20 @@
-class Menu
-	attr_accessor :name
-	attr_accessor :price
-	
-	# initializeメソッドを書き換えてください
-	def initialize(name: ,price:)
-	  self.name = name
-	  self.price = price
-	end
-	
-	def info
-	  return "#{self.name} #{self.price}円"
-	end
-	
-	def get_total_price(count)
-	  total_price = self.price * count
-	  if count >= 3
-		total_price -= 100
-	  end
-	  return total_price
-	end
-  end
+require "./menu"
+
+menu1 = Menu.new(name: "ピザ", price: 800)
+menu2 = Menu.new(name: "すし", price: 1000)
+menu3 = Menu.new(name: "コーラ", price: 300)
+menu4 = Menu.new(name: "お茶", price: 200)
+
+menus = [menu1, menu2, menu3, menu4]
+
+# 変数indexを定義して「0」を代入してください
+index = 0
+
+menus.each do |menu|
+  # 番号をつけてメニューの内容が出力されるように書き換えてください
+  puts "#{index}. #{menu.info}"
   
-  # 引数を渡してインスタンスを生成してください
-  menu1 = Menu.new(name:"すし", price: 1000)
-  
-  puts menu1.info
-  
+  # 変数indexに1を加えて値を更新してください
+  index += 1
+end
+
