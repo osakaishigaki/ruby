48263b8ba3b089b85be468278280d4bfb454085f
mba <mba@mba.local>
2019-11-26 22:54:03 +0900

    113

--- index.rb
+++ index.rb
@@ -1,13 +1,50 @@
-def discount(price)
-	# 「price / 2」を戻り値として返してください
-	return price/2
-end
-
-puts "テレビがセール中です！"
-
-# discountメソッドの戻り値を変数half_priceに代入してください
-half_price = discount( 15000)
-
-# 「特別価格で〇〇円です」となるように出力してください
-
-puts "特別価格で#{half_price}円です"
+class Menu
+	attr_accessor :name
+	attr_accessor :price
+  end
+  
+  menu1 = Menu.new
+  
+  menu1.name = "ピザ"
+  puts menu1.name
+  
+  # menu1のpriceに「800」を代入してください
+  menu1.price =800
+  class Menu
+	attr_accessor :name
+	attr_accessor :price
+  end
+  
+  menu1 = Menu.new
+  
+  menu1.name = "ピザ"
+  puts menu1.name
+  
+  menu1.price = 800
+  puts menu1.price
+  
+  # Menuクラスのインスタンスを生成して変数menu2に代入してください
+  menu2 = Menu.new
+  
+  menu2.name = "すし"
+  puts menu2.name
+  
+  menu2.price = 1000
+  puts menu2.price
+  
+  
+  # menu2のnameに「すし」を代入してください
+  
+  
+  # menu2のnameを出力してください
+  
+  
+  # menu2のpriceに「1000」を代入してください
+  
+  
+  # menu2のpriceを出力してください
+  
+  
+  # menu1のpriceを出力してください
+  puts menu1.price
+  
